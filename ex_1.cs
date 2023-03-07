b1761f75dcdc321a6ec0cca12a5493bf9794607f
Giovana Piorino <giovanapiorino92@gmail.com>
2023-03-07 09:19:38 -0300

    alterações

--- ex_1.cs
+++ ex_1.cs
@@ -1,18 +1,22 @@
 //modulos
-using System;    //import the system namespace, em c++ seria using namespace std
-using System.Collections.Generic;
+using System;    //manipular do SO do usuario
+using System.Collections.Generic; //estruturas de dados
+using System.Linq; //grandes conjuntos de dados
+using System.Text; //manipulação de textos
+using System.Threading.Tasks; //rodas em varios nucleos do processador ao mesmo tempo, multi-threading
+using System.IO; //manipulação de arquivos
 
 
 namespace program
 {
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        int x = 13*1;
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            int x = 13*1;
 
-        Console.WriteLine(x);   //pra nao precisar usar system antes
-    }  
-}
+            Console.WriteLine(x);   //pra nao precisar usar system antes
+        }  
+    }
 }
---
+++ ex_3.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace program
+{
+	class program
+	{
+		public static Main(string[] args)
+		{	
+			//talvez uma forma de polimorfismo de coerção
+			int x = 12345; // int is a 32-bit integer
+			long y = x; // Implicit conversion to 64-bit integer
+			short z = (short)x; // Explicit conversion to 16-bit integer
+			//somente tipos de classe poder ser null, tipos primitivos não podem
+
+		}
+	}
+}
