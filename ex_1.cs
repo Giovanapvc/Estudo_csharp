daf7dbba887cfe1543f5361a13c89c4cf36fe234
Giovana Piorino <giovanapiorino92@gmail.com>
2023-03-07 08:50:04 -0300

    primeiros arquivos de teste

---
+++ ex_1.cs
@@ -0,0 +1,18 @@
+//modulos
+using System;    //import the system namespace, em c++ seria using namespace std
+using System.Collections.Generic;
+
+
+namespace program
+{
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = 13*1;
+
+        Console.WriteLine(x);   //pra nao precisar usar system antes
+    }  
+}
+}
---
+++ ex_2.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace program
+{
+	class program
+	{
+		static void Main(string[] args)
+		{
+		Console.WriteLine("Here, there and everywhere :)");
+		Console.ReadLine(); //usuario escrever qualquer coisa, comando input
+		}
+	}
+}
